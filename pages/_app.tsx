b791d71e4288a0f02dddbe013fbd8cd0a13bbe34
gwanhun <apple@APPLEui-MacBookPro.local>
2023-01-04 00:29:35 +0900

    ✨feat :create responsive mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Layout from 'components/layout';
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
+import Nav from 'components/nav';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Nav />
       <Component {...pageProps} />
     </Layout>
   );
