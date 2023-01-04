273c9b2958c7ffad5a1029a9a7db419d159688bb
choigus98 <cxz32134@gmail.com>
2023-01-04 17:56:21 +0900

    ✨feat : ListPage Layout 제작

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from 'components/layout';
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
 import Nav from 'components/nav';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <Layout>
-      <Nav />
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Nav />
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
